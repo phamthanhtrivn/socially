83fb6fcfcc7e1daab73bafdbf7e9a8623bbe3bb7
Tri Pham <phamthanhtri0712@gmail.com>
2025-07-23 14:04:40 +0700

    suggested users

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -2,6 +2,7 @@
 
 import { prisma } from "@/lib/prisma";
 import { auth, currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 
 // This function is used to sync the user data from Clerk to the database
 export async function syncUser() {
@@ -62,4 +63,89 @@ export async function getDbUserId() {
   if (!user) throw new Error("User not found");
 
   return user.id;
+}
+
+export async function getRandomUsers() {
+  try {
+    const userId = await getDbUserId();
+
+    // get 3 random users exclude ourselves & users that we already follow
+    const randomUsers = await prisma.user.findMany({
+      where: {
+        AND: [
+          { NOT: { id: userId } },
+          { NOT: { followers: { some: { followerId: userId } } } },
+        ],
+      },
+      select: {
+        id: true,
+        name: true,
+        username: true,
+        image: true,
+        _count: {
+          select: {
+            followers: true,
+          },
+        },
+      },
+      take: 5,
+    });
+
+    return randomUsers;
+  } catch (error) {
+    console.log("Error fetching random users: ", error);
+    return [];
+  }
+}
+
+export async function toggleFollow(targetUserId: string) {
+  try {
+    const userId = await getDbUserId();
+
+    if (targetUserId === userId) throw new Error("You cannot follow yourself");
+
+    const existingFollow = await prisma.follows.findUnique({
+      where: {
+        followerId_followingId: {
+          followerId: userId,
+          followingId: targetUserId,
+        },
+      },
+    });
+
+    if (existingFollow) {
+      // unfollow the user
+      await prisma.follows.delete({
+        where: {
+          followerId_followingId: {
+            followerId: userId,
+            followingId: targetUserId,
+          },
+        },
+      });
+    } else {
+      await prisma.$transaction([
+        prisma.follows.create({
+          data: {
+            followerId: userId,
+            followingId: targetUserId,
+          },
+        }),
+
+        prisma.notification.create({
+          data: {
+            type: "FOLLOW",
+            userId: targetUserId, // user being followed
+            creatorId: userId, // user who followed
+          },
+        }),
+      ]);
+    }
+
+    revalidatePath("/")
+    return { success: true };
+  } catch (error) {
+    console.log("Error toggling follow: ", error);
+    return { success: false, error: "Error toggling follow" };
+  }
 }
--- src/components/WhoToFollow.tsx
+++ src/components/WhoToFollow.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { getRandomUsers } from "@/actions/user.action";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import Link from "next/link";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import FollowButton from "./FollowButton";
 
-const WhoToFollow = () => {
+const WhoToFollow = async () => {
+  const users = await getRandomUsers();
+
+  if (users.length === 0) return null;
   return (
-    <div>WhoToFollow</div>
-  )
-}
+    <Card>
+      <CardHeader>
+        <CardTitle>Who to Follow</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="space-y-4">
+          {users.map((user) => (
+            <div
+              key={user.id}
+              className="flex items-center justify-between gap-2 "
+            >
+              <div className="flex items-center gap-5">
+                <Link href={`/profile/${user.username}`}>
+                  <Avatar>
+                    <AvatarImage src={user.image ?? "/avatar.png"} />
+                  </Avatar>
+                </Link>
+                <div className="text-xs">
+                  <Link
+                    href={`/profile/${user.username}`}
+                    className="font-medium cursor-pointer"
+                  >
+                    {user.name}
+                  </Link>
+                  <p className="text-muted-foreground">@{user.username}</p>
+                  <p className="text-muted-foreground">
+                    {user._count.followers} followers
+                  </p>
+                </div>
+              </div>
+              <FollowButton userId={user.id} />
+            </div>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
 
-export default WhoToFollow
+export default WhoToFollow;
